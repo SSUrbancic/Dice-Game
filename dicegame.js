a82adea7a327c3c36d5c7cc1ba5a6a3c49136b4d
Sam <SSUrbancic@outlook.com>
2018-06-13 08:44:25 -0500

    Added two functions with corresponding buttons to attack the other player and to disable the users buttons after their turn allowing the oppenent to attack. Added two supercharge buttons that will give players the opportunity for double damage next turn. Need to create functions in order to enable this feature.

--- dicegame.js
+++ dicegame.js
@@ -1,7 +1,3 @@
-function rollTheDice (numberOfSidesOnDice){
-	let rollOfTheDice = Math.floor(Math.random() * numberOfSidesOnDice) + 1;
-	return rollOfTheDice;
-}
 // console.log(rollTheDice(20));
 // console.log(rollTheDice(6));
 // console.log(rollTheDice(8));
@@ -13,37 +9,51 @@ function rollTheDice (numberOfSidesOnDice){
 
 // let firstPlayer = getPlayerInput("Player One's Name:");
 // let secondPlayer = getPlayerInput("Player Two's Name:");
-// // let thirdPlayer = getPlayerInput("Player Name:");
-// // let fourthPlayer = getPlayerInput("Player Name:");
 
 // console.log(firstPlayer + " is Player One!");
 // console.log(secondPlayer + " is Player Two!");
+
+let playerOne = {
+	health: 100,
+}
+
+let playerTwo = {
+	health: 100,
+}
+
+
+
+
+
+
+
+
+function rollTheDice (numberOfSidesOnDice){
+	let rollOfTheDice = Math.floor(Math.random() * numberOfSidesOnDice) + 1;
+	return rollOfTheDice;
+}
+
 
 function getPlayerInput (question) {
 	playerInput = prompt(question);
 	return playerInput;
 }
 
-let firstTurn = determineFirstTurn();
+// let firstTurn = determineFirstTurn();
+// console.log(firstTurn + " Attacks First!!");
 
-function determineFirstTurn(firstTurn){
-	let firstPlayer = rollTheDice(20);
-	let secondPlayer = rollTheDice(20);
+// function determineFirstTurn(firstTurn){
+// 	let firstPlayer = rollTheDice(20);
+// 	let secondPlayer = rollTheDice(20);
 
-if (firstPlayer > secondPlayer) {
-	firstTurn = "First Player";
-	console.log("First player attacks first!");
-}
-	else {
-	firstTurn = "Second Player";
-	console.log("Second player attacks first!");
-}
-return firstTurn;
-}
-
-
-
-
+// if (firstPlayer > secondPlayer) {
+// 	firstTurn = "First Player";
+// }
+// 	else {
+// 	firstTurn = "Second Player";
+// }
+// return firstTurn;
+// }
 
 function determineAttackPoints(){
 	let attackPoints = rollTheDice(20) + rollTheDice(12) + rollTheDice(4);
@@ -57,47 +67,57 @@ function determineDefensePoints(){
 
 function determineDamageDealt(){
 	let damageDealt = determineAttackPoints() - determineDefensePoints();
-	console.log(damageDealt);
 	return damageDealt;
 }
 
-
 function playerOneAttack(){
+	let playerOneAttackButton = document.getElementById("playerOneAttackButton");
 	let playerOneAttack = determineDamageDealt();
+	let playerOneSuperCharge = document.getElementById("superChargePlayerOneButton");
+	let playerTwoAttackButton = document.getElementById("playerTwoAttackButton");
+	let playerTwoSuperCharge = document.getElementById("superChargePlayerTwoButton");
+
 	if (playerOneAttack > 0) {
 		playerTwo.health = playerTwo.health - playerOneAttack;
 	}
 	else {
 	playerOneAttack = 0;
 	}
-	console.log(playerTwo.health)
-	return playerTwo.health
+	playerOneAttackButton.disabled = true
+	playerOneSuperCharge.disabled = true
+	playerTwoAttackButton.disabled = false
+	playerTwoSuperCharge.disabled = false
+	console.log("Player Two health: " + playerTwo.health);
+	return playerOneAttack
 	}
 
-
-
-function playerTwoAttack(){
+	function playerTwoAttack(){
+	let playerTwoAttackButton = document.getElementById("playerTwoAttackButton");
 	let playerTwoAttack = determineDamageDealt();
+	let playerTwoSuperCharge = document.getElementById("superChargePlayerTwoButton");
+	let playerOneAttackButton = document.getElementById("playerOneAttackButton");
+	let playerOneSuperCharge = document.getElementById("superChargePlayerOneButton");
 	if (playerTwoAttack > 0) {
 		playerOne.health = playerOne.health - playerTwoAttack;
 	}
 	else {
 	playerTwoAttack = 0;
 	}
-	console.log(playerOne.health)
-	return playerOne.health
+	playerTwoAttackButton.disabled = true
+	playerTwoSuperCharge.disabled = true
+	playerOneAttackButton.disabled = false
+	playerOneSuperCharge.disabled = false
+	console.log("Player One health: " + playerOne.health);
+	return playerTwoAttack
 	}
 
 
 
 
-let playerOne = {
-	health: 100,
-}
 
-let playerTwo = {
-	health: 100,
-}
+
+
+
 
 
 //starting the game first player will roll the dice which will determine attach power
